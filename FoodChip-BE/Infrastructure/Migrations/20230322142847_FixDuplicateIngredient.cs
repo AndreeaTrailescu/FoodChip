1eee48bb65bd5e97d2170b97965a6121de47ecaf
ghecatalina <gheorghioiu.catalina@gmail.com>
2023-03-22 16:30:24 +0200

    fix for duplicate ingredient

---
+++ FoodChip-BE/Infrastructure/Migrations/20230322142847_FixDuplicateIngredient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDuplicateIngredient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Bread");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Butter");
+        }
+    }
+}
