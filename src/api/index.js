35a14f50b4bb1bf799f2acafd3f04ae5dd4fc571
Maria <ruviniantumaria@yahoo.com>
2023-04-02 15:59:16 +0300

    api connection

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const api = axios.create({ baseURL: 'https://localhost:7025/' });
+
+export const getCategories = () => api.get('categories');
+export const getCookingMethods = () => api.get('cooking-methods');
+export const getIngredients = () => api.get('ingredients');
+export const getRecipes = (categoryId, cookingMethodId, ingredientIds) => {
+    var path = `recipes?`;
+    if (categoryId != null){
+        path = path.concat(`CategoryId=${categoryId}`);
+    }
+    if (categoryId != null && cookingMethodId != null){
+        path = path.concat(`&CookingMethoId=${cookingMethodId}`);
+    }
+    if (categoryId == null && cookingMethodId != null){
+        path = path.concat(`?CookingMethoId=${cookingMethodId}`);
+    }
+    ingredientIds.map(id => {
+        path = path.concat(`&IngredientIds=${id}`)
+    })
+    return api.get(path);
+}
